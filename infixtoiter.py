74aaa8556abf5b7dfd8553f619add9cd7b1f2807
LeoLeoni <jfleone97@gmail.com>
2017-02-17 00:33:49 -0500

    finished

--- infixtoiter.py
+++ infixtoiter.py
@@ -5,17 +5,16 @@ from newsplit import new_split_iter
 def to_postfix (expr):
 	return postfix_sum(Peekable(new_split_iter(expr)))
 
-def postfix_sum(iterator):
+def postfix_sum (iterator):
 
 	yield from postfix_product(iterator)
+		
+	while peek(iterator) == '+' or peek(iterator) == '-':
+		sign = next(iterator)	#should be an operator sign
+		yield from postfix_product(iterator)
+		yield sign
 
-	while peek(iterator) != ';':
-		if peek(iterator) == '+' or peek(iterator) == '-':
-			sign = next(iterator)	#should be an operator sign
-			yield from postfix_product(iterator)
-			yield sign
-
-def postfix_product(iterator):
+def postfix_product (iterator):
 
 	yield from postfix_factor(iterator)
 
