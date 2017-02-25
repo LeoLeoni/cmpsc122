54a485fbd1bc61672c5289c09675b71c2c4f7516
LeoLeoni <jfleone97@gmail.com>
2017-02-25 13:52:08 -0500

    support for variables

--- newsplit.py
+++ newsplit.py
@@ -12,19 +12,26 @@ def new_split_iter( expr ):
 
 	while expr[pos] != ";":
 
-		if expr[pos].isdigit() == False:
-			#the only non-digits in the expression should be + - * / ( ) so they are yielded as they are
-			yield (expr[pos])
+		if not expr[pos].isalnum():
+			#the only non-alnums in the expression should be + - * / ( ) = so they are yielded as they are
+			yield expr[pos]
 			pos += 1
+
+		elif not expr[pos].isdigit():
+			#support for variables that start with a letter
+			while expr[pos].isalnum():	#will not work if the variable name starts with a number
+				num += expr[pos]
+				pos += 1
+			yield num
+			num = ""
 
 		else:
 			#use string addition to add the digits of the number together, then yield that number as an int. reset the number string after
 			while expr[pos].isdigit():
-				
 				num += expr[pos]
 				pos += 1
 		
-			yield (int(num))
+			yield int(num)
 			num = ""
 
 	yield ";"
